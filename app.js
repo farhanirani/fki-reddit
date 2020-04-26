a6199e81ff1e4f12237bd3210f2fa44350d614ef
Farhan Irani <farhanirani007@gmail.com>
2020-04-26 21:54:40 +0530

    nice

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+require('dotenv').config()
 
+
+//init app
 const app = express()
+//load view engine
 app.set('view engine','pug')
-
-// //body parser
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res) => {
-    res.render('index',{
-        title:'Home',
-        // post: post
-    })
-    
+    res.render('index')
 })
 
 
@@ -34,18 +35,14 @@ app.use('/users', users)
 
 
 
-
-
-
-
-
-
 //start server
 const PORT = process.env.PORT || 3000
-
-// mongoose.connect(process.env.URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-// 	.then(()=>console.log("DB Connected!"))
-// 	.catch(err=>console.log(err))
+//connect db
+mongoose.connect(process.env.URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+})
+	.then(()=>console.log("DB Connected!"))
+	.catch(err=>console.log(err))
 
 app.listen(PORT, function(){
     console.log(`server started on port ${PORT}`)
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,49 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+//bring in user models
+let User = require('../models/user')
+
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+router.post('/signup', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const username = req.body.username
+    const password = req.body.password
+    const password2 = req.body.password2
+
+    let newUser = new User({
+        name:name,
+        email:email,
+        username:username,
+        password:password
+    })
+
+    newUser.save(function(err){ 
+        if(err) {
+            console.log(err)
+            return
+        } else {
+            console.log('reistered succ')
+            res.redirect('/users/login')
+        }
+    })
+
+})
 
 
 
+
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
 
 
 

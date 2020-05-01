75eb43b0b6ae0d092e06637c2b13c47db15f2b07
Farhan Irani <farhanirani007@gmail.com>
2020-05-01 19:53:09 +0530

    .

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,48 @@
 const express = require('express')
 const router = express.Router()
 
+//bring in post models
+let Post = require('../models/post')
+//bring in user models
+let User = require('../models/user')
 
 
+router.get('/create', function(req, res){
+    res.render('create_post')
+})
 
+router.post('/create', function(req, res){
+    console.log(req.body.body)
 
-
-
+    // req.checkBody('title','Title cannot be empty').notEmpty()
+    // // req.checkBody('author','Author cannot be empty').notEmpty()
+    // req.checkBody('body','Body cannot be empty').notEmpty()
+    // //get errors
+    // let errors = req.validationErrors()
+    
+    // if(errors){
+    //     res.render('add',{
+    //         title: 'add article',
+    //         errors:errors
+    //     })
+    // } else {
+    //     //new mongodb schema item
+    //     let article = new Article()
+    //     article.title = req.body.title
+    //     article.author = req.user._id
+    //     article.body = req.body.body
+        
+    //     article.save(function(err){ //to add to db
+    //         if(err) {
+    //             console.log(err)
+    //             return
+    //         } else {
+    //             req.flash('success','Article added')
+    //             res.redirect('/')
+    //         }
+    //     })
+    // }   
+})
 
 
 module.exports = router
